217ffa9ccf38f18ee2c052a819754f0fb6b3d5d3
Minh Nguyen <nguyen_ba_minh@yahoo.com>
2019-08-07 16:40:37 -0700

    08/07/2019 4:40pm

--- application.py
+++ application.py
@@ -1,31 +1,50 @@
 import os
 
-from flask import Flask, session
-from flask_session import Session
+from flask import Flask, render_template, request, redirect, url_for
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 app = Flask(__name__)
 
-# Check for environment variable
-if not os.getenv("DATABASE_URL"):
-    raise RuntimeError("DATABASE_URL is not set")
-
-# Configure session to use filesystem
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
-
-# Set up database
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
-import requests
-res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": "R8JVn8Vq2SAR2QIjGgCEtg", "isbns": "9781632168146"})
-print(res.json())
+@app.route("/")
+def login():
+    return render_template("index.html")
 
-# =============================================================================
-# @app.route("/")
-# def index():
-#     return "Project 1: TODO"
-# =============================================================================
+@app.route("/authenthicate", methods=["POST"])
+def authenthicate():
+    if request.form.get("action") == 'Login':
+        email=request.form.get("email")
+        password=request.form.get("password")
+        if (db.execute("SELECT * FROM users WHERE email = :email and password = :password", {"email": email, "password": password}).rowcount == 0):
+            return render_template("error.html", message="No such user / invalid password.")
+        else:
+            return render_template("booksearch.html")
+    else: #Register
+        email=request.form.get("email")
+        password=request.form.get("password")
+        if (db.execute("SELECT * FROM users WHERE email = :email", {"email": email}).rowcount > 0):
+            return render_template("error.html", message="User already registered.")
+        else:
+            db.execute("INSERT INTO users (email, password) VALUES (:email, :password)",{"email": email, "password": password})
+            db.commit()
+            return render_template("booksearch.html")
+
+@app.route("/booksearch", methods=["POST"])
+def booksearch():
+    isbn=request.form.get("isbn")
+    title=request.form.get("title")
+    author=request.form.get("author")
+    search=''
+    if isbn is None:
+        isbn='%'
+    if title is None:
+        title='%'
+    if author is None:
+        author='%'
+    booklist=db.execute("SELECT * FROM books WHERE (isbn like :isbn) and (title like :title) and (author like :author)",
+                         {"isbn": isbn, "title": title, "author": author}).fetchall()
+    return isbn
+    #render_template("booklist.html", booklist=booklist)
---
+++ create_tables.sql
@@ -0,0 +1,104 @@
+
+Install postgres and misc associated lib
+sudo su -
+apt-get install postgresql postgresql-contrib
+su - postgres  #log in as the oracle user
+psql  #sqlplus
+\q # exit sqlplus
+
+Create postgres user shine
+postgres@Ubuntu18Base:~$ createuser --interactive --pwprompt
+Enter name of role to add: shine
+Enter password for new role:
+Enter it again:
+Shall the new role be a superuser? (y/n) y
+
+You should be able to see the user in the db now:
+postgres@Ubuntu18Base:~$ psql
+postgres=# \du
+                                   List of roles
+ Role name |                         Attributes                         | Member of
+-----------+------------------------------------------------------------+-----------
+ postgres  | Superuser, Create role, Create DB, Replication, Bypass RLS | {}
+ shine     | Superuser, Create role, Create DB                          | {}
+
+Create db under that new user:
+postgres=# create database bookreview owner=shine;
+CREATE DATABASE
+postgres=# \l
+                                  List of databases
+    Name    |  Owner   | Encoding |   Collate   |    Ctype    |   Access privileges
+------------+----------+----------+-------------+-------------+-----------------------
+ bookreview | shine    | UTF8     | en_US.UTF-8 | en_US.UTF-8 |
+ postgres   | postgres | UTF8     | en_US.UTF-8 | en_US.UTF-8 |
+ template0  | postgres | UTF8     | en_US.UTF-8 | en_US.UTF-8 | =c/postgres          +
+            |          |          |             |             | postgres=CTc/postgres
+ template1  | postgres | UTF8     | en_US.UTF-8 | en_US.UTF-8 | =c/postgres          +
+            |          |          |             |             | postgres=CTc/postgres
+(4 rows)
+
+Log into that db (without a corresponding shine unix user)
+postgres@Ubuntu18Base:~$ psql --host=localhost --dbname=bookreview --username=shine
+
+If you want to log in using "psql -U shine" then you need to create a unix user first
+	adduser shine
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    email    VARCHAR UNIQUE NOT NULL,
+    password VARCHAR NOT NULL,
+    created  DATE NOT NULL DEFAULT NOW(),
+    updated   DATE NOT NULL DEFAULT NOW(),
+    deleted  DATE DEFAULT NULL
+);
+
+CREATE TABLE books (
+    isbn     VARCHAR PRIMARY KEY,
+    title    VARCHAR NOT NULL,
+    author   VARCHAR NOT NULL,
+    year     SMALLINT NOT NULL,
+    created  DATE NOT NULL DEFAULT NOW(),
+    updated   DATE NOT NULL DEFAULT NOW(),
+    deleted  DATE DEFAULT NULL
+);
+
+bookreview=# \d
+            List of relations
+ Schema |     Name     |   Type   | Owner
+--------+--------------+----------+-------
+ public | books        | table    | shine
+ public | users        | table    | shine
+ public | users_id_seq | sequence | shine
+(3 rows)
+
+bookreview=# \d books
+                     Table "public.books"
+ Column  |       Type        | Collation | Nullable | Default
+---------+-------------------+-----------+----------+---------
+ isbn    | character varying |           | not null |
+ title   | character varying |           | not null |
+ author  | character varying |           | not null |
+ year    | smallint          |           | not null |
+ created | date              |           | not null | now()
+ updated | date              |           | not null | now()
+ deleted | date              |           |          |
+Indexes:
+    "books_pkey" PRIMARY KEY, btree (isbn)
+
+bookreview=# \d users
+                                  Table "public.users"
+  Column  |       Type        | Collation | Nullable |              Default
+----------+-------------------+-----------+----------+-----------------------------------
+ id       | integer           |           | not null | nextval('users_id_seq'::regclass)
+ username | character varying |           | not null |
+ password | character varying |           | not null |
+ created  | date              |           | not null | now()
+ updated  | date              |           | not null | now()
+ deleted  | date              |           |          |
+Indexes:
+    "users_pkey" PRIMARY KEY, btree (id)
+
+export DATABASE_URL=postgres://shine:shine@localhost/bookreview
+export FLASK_APP=application.py
+export FLASK_DEBUG=1
+
+python3 import.py  # make sure you remove the header before import or skip it duriing import
